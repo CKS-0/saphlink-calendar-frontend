0c8df8ea420a8ede6461faa4c903122f714a732a
Chandrakant Sharma <chandu.sharma6@gmail.com>
2023-05-23 18:24:06 +0530

    Sync & Unsync the Accounts

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -13,11 +13,14 @@ const Settings = () => {
         password: '',
     });
     const [message, setMessage] = useState('');
+    const [syncedAccounts, setSyncedAccounts] = useState([]);
+    const [googleAccounts, setGoogleAccounts] = useState([]);
+    const [outlookAccounts, setOutlookAccounts] = useState([]);
 
     useEffect(() => {
         const getUserData = async () => {
             try {
-                const response = await axiosInstance.get('${baseURL}/user/me');
+                const response = await axiosInstance.get(`${baseURL}/user/me`);
                 const { name, username } = response.data;
                 setUserData({ name, username });
             } catch (error) {
@@ -26,6 +29,30 @@ const Settings = () => {
         };
 
         getUserData();
+    }, []);
+
+    useEffect(() => {
+        const getSyncedAccounts = async () => {
+            try {
+                const response = await axiosInstance.get(`${baseURL}/user/calendar/synced`);
+                const syncedAccounts = response.data;
+
+                const googleSyncedAccounts = syncedAccounts.filter(
+                    (account) => account.profile.email.includes('gmail.com')
+                );
+                const outlookSyncedAccounts = syncedAccounts.filter(
+                    (account) => !account.profile.email.includes('gmail.com')
+                );
+
+                setSyncedAccounts(syncedAccounts);
+                setGoogleAccounts(googleSyncedAccounts);
+                setOutlookAccounts(outlookSyncedAccounts);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getSyncedAccounts();
     }, []);
 
     const handleInput = (e) => {
@@ -51,52 +78,116 @@ const Settings = () => {
         }
     };
 
+    const handleGoogleSignOut = async (account) => {
+        try {
+            const response = await axiosInstance.post(
+                `${baseURL}/user/google/calendar/unsync`,
+                {
+                    resourceId: account.resourceId,
+                    calendarId: account.calendarId,
+                }
+            );
+
+        } catch (error) {
+            console.error(error);
+
+        }
+    };
+
+    const handleOutlookSignOut = async (account) => {
+        try {
+            const response = await axiosInstance.post(
+                `${baseURL}/user/outlook/calendar/unsync`,
+                {
+                    resourceId: account.resourceId,
+                    calendarId: account.calendarId,
+                }
+            );
+
+        } catch (error) {
+            console.error(error);
+
+        }
+    };
+
     return (
-        <div className='settings-page-container'>
+        <div className="settings-page-container">
             <NavigationBar />
             <div className="wave"></div>
             <div className="wave"></div>
             <div className="wave"></div>
-            <div className='form-container'>
+            <div className="form-container">
                 <h2>Settings</h2>
-                <form className='user-update-form' onSubmit={handleSubmit}>
-
+                <form className="user-update-form" onSubmit={handleSubmit}>
                     <label htmlFor="name">Name:</label>
-                    <Input type="text" name="name"
-                        value={userData.name}
-                        onChange={handleInput} />
+                    <Input type="text" name="name" value={userData.name} onChange={handleInput} />
                     <label htmlFor="username">Username:</label>
-                    <Input type="text" name="username"
+                    <Input
+                        type="text"
+                        name="username"
                         value={userData.username}
-                        onChange={handleInput} />
+                        onChange={handleInput}
+                    />
                     <label htmlFor="password">New Password:</label>
-                    <Input type="password" name="password"
+                    <Input
+                        type="password"
+                        name="password"
                         value={userData.password}
-                        onChange={handleInput} />
+                        onChange={handleInput}
+                    />
                     <Button type="submit">Update</Button>
                     {message && <p className="error-message">{message}</p>}
                 </form>
             </div>
-            {/* todo check if the user already has synced calendar */}
-            <div className="button-container">
-                <Button
-                    onClick={async () => {
-                        const { data } = await axiosInstance.get(`${baseURL}/user/google`);
-                        window.location.href = data.message;
-                    }}
-                >
-                    Verify with Google
-                </Button>
-                <Button
-                    onClick={async () => {
-                        const { data } = await axiosInstance.get(`${baseURL}/user/outlook`);
-                        window.location.href = data.message;
-                    }}
-                >
-                    Verify with Outlook
-                </Button>
 
-            </div>
+            {/* Display synced Google accounts */}
+            {googleAccounts.length > 0 ? (
+                <div className="synced-accounts-container">
+                    <h2>Synced Google Accounts</h2>
+                    {googleAccounts.map((account) => (
+                        <div key={account._id} className="account-item">
+                            <p>Name: {account.profile.name}</p>
+                            <p>Email: {account.profile.email}</p>
+                            <Button onClick={() => handleGoogleSignOut(account)}>Sign Out</Button>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="button-container">
+                    <Button
+                        onClick={async () => {
+                            const { data } = await axiosInstance.get(`${baseURL}/user/google`);
+                            window.location.href = data.message;
+                        }}
+                    >
+                        Verify with Google
+                    </Button>
+                </div>
+            )}
+
+            {/* Display synced Outlook accounts */}
+            {outlookAccounts.length > 0 ? (
+                <div className="synced-accounts-container">
+                    <h2>Synced Outlook Accounts</h2>
+                    {outlookAccounts.map((account) => (
+                        <div key={account._id} className="account-item">
+                            <p>Email: {account.profile.email}</p>
+                            <Button onClick={() => handleOutlookSignOut(account)}>Sign Out</Button>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div className="button-container">
+                    <Button
+                        onClick={async () => {
+                            const { data } = await axiosInstance.get(`${baseURL}/user/outlook`);
+                            window.location.href = data.message;
+                        }}
+                    >
+                        Verify with Outlook
+                    </Button>
+                </div>
+            )}
         </div>
     );
 };
